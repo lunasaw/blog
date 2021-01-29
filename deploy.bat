cba54f4f4384bfc01c7bf22d1c3051d767331125
luna <iszychen@gmail.com>
2021-01-29 15:06:54 +0800

    'auto-deploy'

--- deploy.bat
+++ deploy.bat
@@ -3,7 +3,7 @@ git init
 git add -A
 git commit -m 'auto-deploy'
 git remote add origin https://gitee.com/luna_nov/blog.git
-git remote add github https://github.com/czy1024/blog.git
+git remote add github git@github.com:czy1024/blog.git
 
 git pull
 git push --force origin HEAD:master
